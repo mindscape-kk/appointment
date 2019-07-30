2115fce60202d7536fb748efefaae58d41747f33
K <hamed.khandan@mscp.co>
2019-07-30 14:55:48 +0900

    Stub code for client error

--- backend/app/co/mscp/appointment/control/ErrorHandler.scala
+++ backend/app/co/mscp/appointment/control/ErrorHandler.scala
@@ -8,11 +8,31 @@ import play.api.mvc.{RequestHeader, Result, Results}
 
 import scala.concurrent.Future
 
+object ErrorHandler {
+  import java.util.regex.Pattern
+  private val JSON_ERROR_PATTERN = Pattern.compile("Error decoding json body:[ \\w\\.]+: (.*)")
+}
+
+
 class ErrorHandler extends HttpErrorHandler {
-  override def onClientError(request: RequestHeader, statusCode: Int,
-    message: String): Future[Result] = null
+  import ErrorHandler._
+
+  override def onClientError(request: RequestHeader, statusCode:
+  Int,message: String): Future[Result] = {
+    // TODO Always log
+    /*
+    Matcher m = JSON_ERROR_PATTERN.matcher(message);
+    if(m.find()) {
+        String msg = m.group(1);
+        throw new HttpError(HttpErrorStatus.NOT_ACCEPTABLE, msg);
+    }
+    throw new HttpError(HttpErrorStatus.of(statusCode),
+        "Bad invocation of endpoint: " + request.toString());
+   */
+  }
 
   override def onServerError(request: RequestHeader, exception: Throwable): Future[Result] =
+    // TODO Always log
     exception match {
       case _: CompletionException => onServerError(request, exception.getCause)
       case e: ServiceError => Future(toResult(e))
