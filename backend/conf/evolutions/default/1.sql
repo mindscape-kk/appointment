2b1141a9bc0198cc1223080c4b12d1a9ef832337
Anuradha G <theanuradha@gmail.com>
2019-12-03 11:31:22 +0530

    added new tables

--- backend/conf/evolutions/default/1.sql
+++ backend/conf/evolutions/default/1.sql
@@ -1,5 +1,7 @@
 # --- !Ups
 CREATE SEQUENCE RESOURCE_SEQ;
+CREATE SEQUENCE TIMESLOT_SEQ;
+CREATE SEQUENCE TIMESLOT_PATTERN_SEQ;
 
 CREATE TABLE "RESOURCE" (
     "ID" text NOT NULL DEFAULT 'RES'||nextval('RESOURCE_SEQ'::regclass)::TEXT,
@@ -12,9 +14,36 @@ CREATE TABLE "RESOURCE" (
     --TODO: flag for record status [eg: delete resource]
 );
 
+CREATE TABLE "TIMESLOT" (
+    "ID" text NOT NULL DEFAULT 'RES'||nextval('TIMESLOT_SEQ'::regclass)::TEXT,
+    "BEGIN" timestamp,
+    "DURATION" integer,
+    "ASSIGNEE" text,
+    "CONFIRMED" boolean,
+    "CANCEL_REQUEST" boolean,
+    PRIMARY KEY ("ID")
+    --TODO: add time stamp for data create/update
+);
+
+CREATE TABLE "TIMESLOT_PATTERN" (
+    "ID" text NOT NULL DEFAULT 'RES'||nextval('TIMESLOT_PATTERN_SEQ'::regclass)::TEXT,
+    "START_DATE" date,
+    "END_DATE" date,
+    "START_TIME" timestamp,
+    "DURATION" integer,
+    "REPEAT_PER_DAY" integer,
+    "REPEAT_ON_DAYS" text,
+    PRIMARY KEY ("ID")
+    --TODO: add time stamp for data create/update
+);
+
 
 
 
 # --- !Downs
 DROP TABLE IF EXISTS RESOURCE CASCADE;
+DROP TABLE IF EXISTS TIMESLOT CASCADE;
+DROP TABLE IF EXISTS TIMESLOT_PATTERN CASCADE;
 DROP SEQUENCE IF EXISTS RESOURCE_SEQ;
+DROP SEQUENCE IF EXISTS TIMESLOT_SEQ;
+DROP SEQUENCE IF EXISTS TIMESLOT_PATTERN_SEQ;
